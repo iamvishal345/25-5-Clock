4ba2476a3c039cabba21cb7da1811b178c3fb93a
iamvishal345 <vishalagnihotri345@gmail.com>
2020-12-24 21:40:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.scss";
+import { DisplayControl } from "./components/display-control";
+import { Display } from "./components/display";
+import { TimeControl } from "./components/timer-control";
 
 function App() {
+  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
+  const [isRunning, setIsRunning] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  const handleClick = useCallback(
+    (e) => {
+      if (isRunning) return;
+      const id = e.target.id;
+      switch (id) {
+        case "break-decrement":
+          breakLength > 1 && setBreakLength(breakLength - 1);
+          break;
+        case "break-increment":
+          breakLength < 60 && setBreakLength(breakLength + 1);
+          break;
+        case "session-decrement":
+          sessionLength > 1 && setSessionLength(sessionLength - 1);
+          break;
+        case "session-increment":
+          sessionLength < 60 && setSessionLength(sessionLength + 1);
+          break;
+        default:
+          break;
+      }
+    },
+    [breakLength, sessionLength, isRunning]
+  );
+  const handleResetTimer = () => {
+    setBreakLength(5);
+    setSessionLength(25);
+    setReset(!reset);
+    setIsRunning(false);
+  };
+  const handleToggleTimer = () => {
+    setIsRunning(!isRunning);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">25 + 5 Clock</header>
+      <section>
+        <DisplayControl
+          prefix="break"
+          value={breakLength}
+          handleClick={handleClick}
+        />
+        <DisplayControl
+          prefix="session"
+          value={sessionLength}
+          handleClick={handleClick}
+        />
+      </section>
+      <Display
+        sessionLength={sessionLength}
+        breakLength={breakLength}
+        isRunning={isRunning}
+        resetEvent={reset}
+      />
+      <TimeControl
+        resetTimer={handleResetTimer}
+        toggleTimer={handleToggleTimer}
+      />
     </div>
   );
 }
